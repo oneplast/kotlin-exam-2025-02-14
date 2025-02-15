de1f1f75c9de2ed09bcd1857a1ab8761c3a12da8
Gyeonghun Min <riverpower6@naver.com>
2025-02-15 23:38:34 +0900

    [FEAT] [8단계] 수정

--- src/main/kotlin/org/example/domain/wiseSaying/wiseSaying/controller/WiseSayingController.kt
+++ src/main/kotlin/org/example/domain/wiseSaying/wiseSaying/controller/WiseSayingController.kt
@@ -28,7 +28,7 @@ class WiseSayingController {
         println("번호 / 작가 / 명언")
         println("----------------------")
 
-        wiseSayings.forEach{
+        wiseSayings.forEach {
             println("${it.id} / ${it.author} / ${it.content}")
         }
     }
@@ -41,15 +41,43 @@ class WiseSayingController {
             return
         }
 
-        val wiseSaying = wiseSayings.firstOrNull {it.id == id}
+        val wiseSaying = wiseSayings.firstOrNull { it.id == id }
 
         if (wiseSaying == null) {
-            println("해당 id의 명언은 존재하지 않습니다.")
+            println("${id}번 명언은 존재하지 않습니다.")
             return
         }
 
         wiseSayings.remove(wiseSaying)
 
         println("${id}번 명언을 삭제하였습니다.")
+    }
+
+    fun actionModify(rq: Rq) {
+        val id = rq.getParamValueAsInt("id", 0)
+
+        if (id == 0) {
+            println("id를 정확히 입력해주세요.")
+            return
+        }
+
+        val wiseSaying = wiseSayings.firstOrNull{it.id == id}
+
+        if (wiseSaying == null) {
+            println("${id}번 명언은 존재하지 않습니다.")
+            return
+        }
+
+        println("명언(기존) : ${wiseSaying.content}")
+        print("명언 : ")
+        val content = readlnOrNull()!!.trim()
+
+        println("작가(기존) : ${wiseSaying.author}")
+        print("작가 : ")
+        val author = readlnOrNull()!!.trim()
+
+        wiseSaying.update(content, author)
+
+        println("${id}번 명언을 수정하였습니다.")
     }
 }
