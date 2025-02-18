4c9e2c3d1a37e2fad7ebbbabc43eca872fd0a890
Gyeonghun Min <riverpower6@naver.com>
2025-02-18 16:26:58 +0900

    [REFACTOR] Repository 인터페이스화 및 분리

--- src/main/kotlin/org/example/global/bean/SingletonScope.kt
+++ src/main/kotlin/org/example/global/bean/SingletonScope.kt
@@ -2,12 +2,13 @@ package org.example.global.bean
 
 import org.example.domain.system.system.controller.SystemController
 import org.example.domain.wiseSaying.wiseSaying.controller.WiseSayingController
+import org.example.domain.wiseSaying.wiseSaying.repository.WiseSayingMemoryRepository
 import org.example.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository
 import org.example.domain.wiseSaying.wiseSaying.service.WiseSayingService
 
 object SingletonScope {
     val wiseSayingController by lazy { WiseSayingController() }
     val wiseSayingService by lazy { WiseSayingService() }
-    val wiseSayingRepository by lazy { WiseSayingRepository() }
+    val wiseSayingRepository: WiseSayingRepository by lazy { WiseSayingMemoryRepository() }
     val systemController by lazy { SystemController() }
 }
